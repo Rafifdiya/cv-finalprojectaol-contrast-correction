9159348d833251a1fd5d9eb4e8dba5c81ca3607e
Rafifdiya <rafifdiya@binus.ac.id>
2026-06-22 10:58:57 +0700

    Remove DEMO_PACKAGE folder

--- DEMO_PACKAGE/processing/__init__.py
+++
@@ -1,5 +0,0 @@
-# processing/__init__.py
-from processing.ghe import apply_ghe
-from processing.clahe import apply_clahe, run_clahe_grid_experiment
-from processing.metrics import evaluate, calculate_psnr, calculate_ssim
-from processing.color_utils import load_image, save_image, pil_to_bgr, bgr_to_rgb
--- DEMO_PACKAGE/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# utils/__init__.py
-from utils.plot_utils import plot_histogram, plot_comparison_histograms
